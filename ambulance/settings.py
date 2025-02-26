c8c23680e31bd5352ecabaa3d02011e115f96a4f
Abel Louis Fernandez <137674054+AbelLouisFernandez@users.noreply.github.com>
2025-02-26 22:00:37 +0530

    Update settings.py

--- ambulance/settings.py
+++ ambulance/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "abel010705@gmail.com"
-EMAIL_HOST_PASSWORD = "jueq uqqj athz mwqm"  # Use an app password, not your real password
+EMAIL_HOST_USER = "yourgmail@gmail.com"
+EMAIL_HOST_PASSWORD = "apppassword"  # Use an app password, not your real password
